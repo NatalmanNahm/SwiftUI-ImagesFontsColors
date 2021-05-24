ef46ef9d10b454790025a171dbe614d925e5d6a7
Mike Panitz <michpa@hotmail.com>
2021-05-23 20:16:04 -0700

    Initial Commit

---
+++ ImagesFontsColors-01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagesFontsColors-01
+//
+//  Created by Mike Panitz on 5/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImagesFontsColors-01/ImagesFontsColors_01App.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesFontsColors_01App.swift
+//  ImagesFontsColors-01
+//
+//  Created by Mike Panitz on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct ImagesFontsColors_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
